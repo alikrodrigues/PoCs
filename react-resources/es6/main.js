7b195d68b0858e9cf683d1e792b15291f10590b7
Alik Rodrigues <alikorodrigues@gmail.com>
2026-01-15 10:12:21 -0300

    ES6

--- react-resources/es6/main.js
+++ react-resources/es6/main.js
@@ -24,3 +24,51 @@ let id = Symbol("id");
 person[id] = 140353;
 console.log(person.id);
 console.log(person[id]);
+
+// Reflect - para resolver problemas de inconsistencias
+// Captura propriedade com valor "dinamico", para interceptar e resolver problemas atuando com o Proxy
+
+const house = {
+  flatRoof: false,
+  stairs: true,
+  garage: true,
+  pool: false,
+};
+
+console.log("Does the house have a pool ? ", Reflect.get(house, pool));
+
+/// SIMULATE A DYNAMIC PROP SELECT
+const props = Object.keys(house);
+const randomProp = props[Math.floor(Math.random() * props.length)];
+//
+
+console.log("Selected Prop value: ", Reflect.get(house, randomProp));
+
+// Proxy - Interceptar operações como get/set/in/delete...
+
+const user = {
+  name: "Alik",
+  age: 30,
+};
+
+const proxy = new Proxy(user, {
+  get(target, prop) {
+    console.log("Acessando:", prop);
+    // Fazer alguma coisa
+    if (prop === "age") {
+      console.log("Idade exposta ao cliente");
+    }
+    return target[prop];
+  },
+
+  deleteProperty(target, prop) {
+    if (prop === "id") {
+      console.log("❌ Não é permitido deletar o id");
+      return false;
+    }
+    console.log(`[AUDIT] Removendo ${String(prop)}`);
+    return Reflect.deleteProperty(target, prop);
+  },
+});
+
+console.log(proxy.name);
