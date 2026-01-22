88be735328f03586363e47853c9fd44844d45377
Alik Rodrigues <alikorodrigues@gmail.com>
2026-01-22 12:58:18 -0300

    FlatMap

--- react-resources/es6/main.js
+++ react-resources/es6/main.js
@@ -123,4 +123,12 @@ function Entries() {
   //   ]
 }
 
-//Async and Await
+//JS 2019
+//Flat Map
+
+const myArr = [1, 2, 3, 4, 5, 6];
+const newArr = myArr.flatMap((x) => [x, x * 10]);
+console.log(newArr);
+
+const array = [{ items: ["a", "b"] }, { items: ["c", "d"] }];
+console.log(array.flatMap((item) => item.items));
